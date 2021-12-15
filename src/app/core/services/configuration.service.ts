92a1deec5dc3b3cf47733a717e751184183b8554
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-15 19:58:06 -0300

    feat: creating a method on configuration service to get configuration properties

--- src/app/core/services/configuration.service.ts
+++ src/app/core/services/configuration.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs';
+import { Configuration, ImageConfiguration, ImageConfigurationApiResponse } from '../models';
+import { ApiUtilsService } from './api-utils.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigurationService {
 
-  constructor() { }
+  private configuration: Map<string, string> = new Map();
+
+  constructor(private httpClient: HttpClient, private apiUtilsService: ApiUtilsService) { }
+
+  getConfiguration(key: string) {
+    return this.configuration.get(key);
+  }
+
+  setConfiguration() {
+    // @TODO should be destroyed
+    this.httpClient.get<Configuration>(`https://api.themoviedb.org/3/configuration`)
+      .pipe(map((configuration: Configuration) => configuration.images),
+      map((imagesConfiguration: ImageConfigurationApiResponse) => this.apiUtilsService.rewriteProperties<ImageConfiguration>(imagesConfiguration)),
+      tap((imageConfiguration: ImageConfiguration) => {
+        Object.entries(imageConfiguration).forEach(([key, value]) => this.configuration.set(key, value));
+        console.log(this.configuration)
+      }))
+      .subscribe(() => {
+
+      })
+  }
+
 }
