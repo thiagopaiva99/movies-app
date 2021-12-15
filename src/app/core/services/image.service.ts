83fad9d2b79ffbf0ce7af94f16f2a0dd6ad7d518
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-15 19:56:54 -0300

    feat: creating method to return image url by service

--- src/app/core/services/image.service.ts
+++ src/app/core/services/image.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { ConfigurationService } from './configuration.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  constructor() { }
+  constructor(private configurationService: ConfigurationService) { }
+
+  // @TODO validate by resolution
+  getUrl(imagePath: string): string {
+      return this.configurationService.getConfiguration('baseUrl') + 'w154' + imagePath;
+    }
+
 }
