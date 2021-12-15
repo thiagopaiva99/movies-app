eac5cdf1c9790b5a985d22adfe63c58e304c769a
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-15 19:22:42 -0300

    feat: creating method to get movies by category on movies service

--- src/app/core/services/movies.service.ts
+++ src/app/core/services/movies.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Movie, MovieApiResponse, Pagination } from '../models';
+import { map, Observable } from 'rxjs';
+import { ApiUtilsService } from './api-utils.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private apiUtilsService: ApiUtilsService) { }
+
+  getMoviesByCategory(category: string): Observable<Movie[]> {
+    return this.httpClient.get<Pagination<MovieApiResponse>>(`https://api.themoviedb.org/3/movie/${category}`)
+      .pipe(
+        map((value: Pagination<MovieApiResponse>) => value.results),
+      map((response: MovieApiResponse[]) => response.map(value => this.apiUtilsService.rewriteProperties<Movie>(value)))
+      );
+  }
+
 }
