30b2a517453d1f505dd8b18fe60a97205d27ac7c
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-15 19:57:46 -0300

    feat: adding movie image url on get by category on service

--- src/app/core/services/movies.service.ts
+++ src/app/core/services/movies.service.ts
@@ -3,19 +3,20 @@ import { HttpClient } from '@angular/common/http'
 import { Movie, MovieApiResponse, Pagination } from '../models';
 import { map, Observable } from 'rxjs';
 import { ApiUtilsService } from './api-utils.service';
+import { ImageService } from './image.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  constructor(private httpClient: HttpClient, private apiUtilsService: ApiUtilsService) { }
+  constructor(private httpClient: HttpClient, private apiUtilsService: ApiUtilsService, private imageService: ImageService) { }
 
   getMoviesByCategory(category: string): Observable<Movie[]> {
     return this.httpClient.get<Pagination<MovieApiResponse>>(`https://api.themoviedb.org/3/movie/${category}`)
       .pipe(
         map((value: Pagination<MovieApiResponse>) => value.results),
-      map((response: MovieApiResponse[]) => response.map(value => this.apiUtilsService.rewriteProperties<Movie>(value)))
+      map((movies: MovieApiResponse[]) => movies.map(movie => ({ ...this.apiUtilsService.rewriteProperties<Movie>(movie), imageUrl: this.imageService.getUrl(movie.poster_path as string)})))
       );
   }
 
