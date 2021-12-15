823de944ce6136b9275166ca421387f4ec9830c7
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-15 18:12:01 -0300

    feat: creating first version for tab pills display

--- src/app/modules/movies/components/tab-pills/tab-pills.component.ts
+++ src/app/modules/movies/components/tab-pills/tab-pills.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Category } from '../../movies.model';
 
 @Component({
   selector: 'app-tab-pills',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabPillsComponent implements OnInit {
 
+  @Input()
+  public tabs: Map<string, Category> = new Map();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get tabsList() {
+    return Array.from(this.tabs.values());
   }
 
 }
