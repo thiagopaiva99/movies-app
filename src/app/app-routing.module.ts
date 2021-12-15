75b0e73aff7cf41f34309b3688588b2912c86035
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-15 17:46:52 -0300

    feat: creating initial modules for movies and search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'movie/most-popular',
+  },
+  {
+    path: 'movie',
+    loadChildren: () => import('./modules/movies/movies.module').then((m) => m.MoviesModule),
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./modules/search/search.module').then((m) => m.SearchModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
