702297e9171b774ab0f06a63945ad0831b826e3a
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-15 18:23:44 -0300

    feat: creating the infinite scroll module with component

---
+++ src/app/shared/components/infinite-scroll/infinite-scroll.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InfiniteScrollComponent } from './infinite-scroll.component';
+
+
+
+@NgModule({
+  declarations: [
+    InfiniteScrollComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InfiniteScrollModule { }
