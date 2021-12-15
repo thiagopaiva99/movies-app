477c0b158dfa640b22abf2b85c5e9d00f2872e07
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-15 17:47:27 -0300

    feat: creating movies list component

--- src/app/modules/movies/movies.module.ts
+++ src/app/modules/movies/movies.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MoviesRoutingModule } from './movies-routing.module';
+import { MoviesListComponent } from './movies-list/movies-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MoviesListComponent
+  ],
   imports: [
     CommonModule,
     MoviesRoutingModule
