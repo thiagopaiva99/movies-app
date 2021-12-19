6ac9d164237b704d93656205918f0d2bf14aa8ff
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-19 16:03:55 -0300

    feat: adding apiUrl on environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGJlZDIxYjc0MDQ1NGRhNjUwMGQzYWM5NDM0MmE2YyIsInN1YiI6IjVjZTliNTBjOTI1MTQxNTc4YmI5ZWY5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Vg7IWY9aucXvxnqfyFxeZd8J49pBdsZ16PWyB1toD1Y'
+  apiToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGJlZDIxYjc0MDQ1NGRhNjUwMGQzYWM5NDM0MmE2YyIsInN1YiI6IjVjZTliNTBjOTI1MTQxNTc4YmI5ZWY5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Vg7IWY9aucXvxnqfyFxeZd8J49pBdsZ16PWyB1toD1Y',
+  apiUrl: 'https://api.themoviedb.org/3',
 };
 
 /*
