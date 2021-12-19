59699571ea9a7aad41380a1d1a625ca8dd26687f
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-19 16:42:38 -0300

    creating the movie details component

--- src/app/modules/movies/containers/movie-details/movie-details.component.ts
+++ src/app/modules/movies/containers/movie-details/movie-details.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Character, Movie } from 'src/app/core/models';
+import { MoviesService } from '../../../../core/services/movies.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private moviesService: MoviesService, private router: Router) { }
+
+  public movie!: Movie;
+  public cast$!: Observable<Character[]>;
+  public recommendations$!: Observable<Movie[]>;
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(({ id }) => {
+      this.moviesService.getById(+id).subscribe(movie => {
+        this.movie = movie
+      }, (error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          this.router.navigate(['page-not-found'])
+        }
+      })
+
+      this.cast$ = this.moviesService.getCredits(+id)
+      this.recommendations$ = this.moviesService.getRecommendations(+id)
+    })
   }
 
+  onRecommendationClick(movie: Movie) {
+    this.router.navigate(['/movie', movie.id, 'details'], { skipLocationChange: true });
+  }
+
+  /* istanbul ignore next */
+  slidePrevious(element: HTMLElement) {
+    const elementWidth = element.offsetWidth;
+    element.scroll({ left: element.scrollLeft - elementWidth, behavior: 'smooth' });
+  }
+
+  /* istanbul ignore next */
+  slideNext(element: HTMLElement) {
+    const elementWidth = element.offsetWidth;
+    element.scroll({ left: element.scrollLeft + elementWidth, behavior: 'smooth' });
+  }
 }
