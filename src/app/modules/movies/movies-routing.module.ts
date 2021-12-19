fb7089084e119a4a00d8f522dfc8f90ca45b6e8c
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-19 16:48:22 -0300

    feat: creating route for category, details and search on movies module

--- src/app/modules/movies/movies-routing.module.ts
+++ src/app/modules/movies/movies-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailsComponent } from './containers/movie-details/movie-details.component';
 import { MoviesListComponent } from './containers/movies-list/movies-list.component';
+import { MoviesSearchComponent } from './containers/movies-search/movies-search.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'most-popular'
+  },
+  {
+    path: 'search',
+    component: MoviesSearchComponent
+  },
+  {
+    path: ':id/details',
+    component: MovieDetailsComponent
+  },
   {
     path: ':category',
     component: MoviesListComponent
