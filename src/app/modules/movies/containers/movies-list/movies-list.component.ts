367867a7d76e7a0a27fca4856b72c3e011d20c42
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-19 16:44:37 -0300

    feat: creating the movies list page

--- src/app/modules/movies/containers/movies-list/movies-list.component.ts
+++ src/app/modules/movies/containers/movies-list/movies-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first, tap } from 'rxjs';
 import { Movie } from 'src/app/core/models';
-import { MoviesService } from 'src/app/core/services/movies.service';
+import { MoviesService } from '../../../../core/services/movies.service';
 import { Category } from '../../movies.model';
 
 @Component({
@@ -20,18 +20,62 @@ export class MoviesListComponent implements OnInit {
     ['top-rated', { apiValue: 'top_rated', text: 'Top Rated' }],
   ]);
 
-  public movies$!: Observable<Movie[]>;
+  public movies: Movie[] = [];
 
-  constructor(private activatedRoute: ActivatedRoute, private moviesService: MoviesService) { }
+  public isLoadingMoreMovies = true;
+
+  private currentPage = 1;
+
+  constructor(private activatedRoute: ActivatedRoute, private moviesService: MoviesService, private router: Router) { }
 
   ngOnInit(): void {
     this.category = this.activatedRoute.snapshot.params['category'];
 
+    if (!this.categories.has(this.category)) {
+      this.router.navigate(['page-not-found']);
+    }
+
+    this.loadMovies(this.getCurrentCategory(), this.currentPage)
+  }
+
+  private getCurrentCategory() {
     const category = this.categories.has(this.category)
-      ? this.categories.get(this.category)?.apiValue
+      ? this.categories.get(this.category)!.apiValue
       : this.categories.entries().next().value[1].apiValue;
 
-    this.movies$ = this.moviesService.getMoviesByCategory(category);
+    return category;
+  }
+
+  onTabChange(tab: string) {
+    const category = this.categories.get(tab);
+    this.currentPage = 1
+    this.router.navigate(['movie', tab])
+    this.movies = []
+    this.loadMovies(category!.apiValue, this.currentPage)
+  }
+
+  onScroll() {
+    if (this.isLoadingMoreMovies) {
+      return
+    }
+
+    this.currentPage++
+    this.loadMovies(this.getCurrentCategory(), this.currentPage);
+  }
+
+  onMovieClick(movie: Movie) {
+    this.router.navigate(['movie', movie.id, 'details'])
+  }
+
+  onSearch(search: string) {
+    this.router.navigate(['movie', 'search'], { queryParams: { query: search } })
+  }
+
+  private loadMovies(category: string, page: number) {
+    this.isLoadingMoreMovies = true;
+    this.moviesService.getMoviesByCategory(category, page)
+      .pipe( tap(() => this.isLoadingMoreMovies = false), first())
+      .subscribe(movies => this.movies.push(...movies));
   }
 
 }
