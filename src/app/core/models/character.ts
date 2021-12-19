0bbd1d170de84a16ce07b5a15c8dc2340cd8cc2a
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-19 16:35:14 -0300

    feat: creating the model for characters

---
+++ src/app/core/models/character.ts
@@ -0,0 +1,14 @@
+export interface CharacterApiResponse {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string;
+}
+
+export interface Character {
+  id: number;
+  name: string;
+  character: string;
+  profilePath: string;
+  imageUrl: string;
+}
