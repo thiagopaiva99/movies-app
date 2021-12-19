a4485570b964e2e3b339c7baa43168b5c1d041e7
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-19 16:34:57 -0300

    feat: adding more properties to movies model and createing the genre interface

--- src/app/core/models/movie.ts
+++ src/app/core/models/movie.ts
@@ -1,18 +1,29 @@
+import { ListItem } from "src/app/shared/components/list-item/list-item.model";
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
 export interface MovieApiResponse {
+  id: number;
   poster_path?: string;
+  backdrop_path?: string;
   release_date: string;
-  genre_ids: string[]
+  genres: Genre[];
   original_language: string;
   overview: string;
   title: string;
   vote_average: number;
 }
 
-export interface Movie {
+export interface Movie extends ListItem {
+  id: number;
   imageUrl: string;
+  backdropUrl?: string;
   posterPath?: string;
   releaseDate: string;
-  genreIds: string[]
+  genres: Genre[];
   originalLanguage: string;
   overview: string;
   title: string;
