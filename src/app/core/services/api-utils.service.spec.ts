64fe42db237e515e76129132f136969acb0e73be
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-19 16:34:03 -0300

    test: adding unit tests for api utils service

--- src/app/core/services/api-utils.service.spec.ts
+++ src/app/core/services/api-utils.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { lorem } from 'faker';
 
 import { ApiUtilsService } from './api-utils.service';
 
@@ -13,4 +14,15 @@ describe('ApiUtilsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should rewrite object properties from snake to camel case', () => {
+    const object = {
+      property_one: lorem.word(),
+      property_two: lorem.word(),
+    }
+
+    const rewritedObject = service.rewriteProperties(object);
+    expect(rewritedObject).toHaveProperty('propertyOne')
+    expect(rewritedObject).toHaveProperty('propertyTwo')
+  })
 });
